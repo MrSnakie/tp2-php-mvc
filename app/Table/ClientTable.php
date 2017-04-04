6410b84a93df6e8634372f6b7e4f1a6f0d83802e
MrSnakie <petitloulou96@free.fr>
2017-04-04 11:42:22 +0200

    Display client in table

---
+++ app/Table/ClientTable.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Table;
+
+use Core\Table\Table;
+/**
+* 
+*/
+class ClientTable extends Table
+{
+    protected $table = "clients";
+
+    public function byStatutName()
+    {
+        return $this->query(" SELECT clients.id,
+                                     clients.first_name,
+                                     clients.last_name,
+                                     clients.birth_date,
+                                     clients.adress,
+                                     clients.postal_code,
+                                     clients.phone,
+                                     marital_status.status as statut
+                                FROM clients
+                                LEFT JOIN marital_status
+                                       ON marital_status_id = marital_status.id
+                                ORDER BY clients.last_name
+                            ");
+    }
+}
--- pages/index.php
+++ pages/index.php
@@ -1,3 +1,28 @@
 <div class="row">
-    <h3>Travaux en cours...</h3> 
+    <div class="col-md-9">
+        <h2>Liste des clients</h2>
+        <hr class="my-separator">
+        <table class="col-md-12 table table-hover">
+            <thead>
+                <tr>
+                    <th>Nom / Prénom</th>
+                    <th>Âge</th>
+                    <th>Adresse / Code postal</th>
+                    <th>Téléphone</th>
+                    <th>Situation</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach(App::getInstance()->getTable('client')->byStatutName() as $client): ?>
+                    <tr>
+                        <td><?= $client->identity ?></td>
+                        <td><?= $client->age ?></td>
+                        <td><?= $client->adress .' - '. $client->postal_code ?></td>
+                        <td><?= $client->phone ?></td>
+                        <td><?= $client->statut ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div> 
 </div>
